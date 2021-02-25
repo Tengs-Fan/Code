097047ad41dd3662f937c2fe97d7000d7cd69382
Penkwe Tjengh <penkwe@MacOS.local>
2021-02-25 11:19:58 +0800

    apue 4.7

---
+++ Unix/4_File+Directory/apue.h
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#define exit(EXP)	return(EXP)
+#define err_sys(...) { printf(__VA_ARGS__); \
+	return -1;}
+#define err_quit(...) { printf(__VA_ARGS__); \
+	return -1;}
+#define err_dump(...) { printf(__VA_ARGS__); \
+	break ;}
+#define err_ret(...) { printf(__VA_ARGS__); \
+	return -1 ;}
