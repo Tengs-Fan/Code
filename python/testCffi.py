6d67f91903b41b0601ffc465294dc4155370203e
Tengs Penkwe <tengspenkwe@gmail.com>
2022-11-18 11:45:49 -0800

    sync files

---
+++ python/testCffi.py
@@ -0,0 +1,16 @@
+from cffi import FFI
+ffi = FFI()
+ffi.set_source("_test", """
+long factorial(int n) {
+    long r = n;
+    while(n > 1) {
+        n -= 1;
+        r *= n;
+    }
+    return r;
+}
+""")
+ffi.cdef("""long factorial(int);""")
+ffi.compile()
+from _test import lib     # import the compiled library
+print(lib.factorial(100))  # 3628800
